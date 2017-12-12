74c05bb5d7683373f53f638ef7f32363dd333c99
JThunder99 <32822044+JThunder99@users.noreply.github.com>
2017-12-11 23:12:42 -0600

    Codecademy generated from the project submission page.

---
+++ jammming/src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './SearchResults.css';
+import TrackList from '../Tracklist/Tracklist.js';
+
+class SearchResults extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="SearchResults">
+        <h2>Results</h2>
+        <TrackList tracks={this.props.searchResults} onAdd={this.props.onAdd} />
+      </div>
+    )
+  }
+}
+
+export default SearchResults;
